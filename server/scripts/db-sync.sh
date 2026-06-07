8d9e352449a2c5103e29d3b719485eacfc25d309
Gary Yamashita <garyjyam@gmail.com>
2026-06-07 13:31:55 -0700

    fix: too many attempts lockout, use public route in prebuild

--- server/scripts/db-sync.sh
+++ server/scripts/db-sync.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# This is a standard bash script, no YAML escaping needed!
-echo "Running database sync..."
 node -e "require('../models').sequelize.sync({ force: true }).then(() => process.exit(0)).catch(e => { console.error(e); process.exit(1); })"
